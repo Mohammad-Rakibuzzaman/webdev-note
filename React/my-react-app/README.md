48315a621f132e55f12735115c0c5402aa9690d7
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-03-12 04:33:23 +0600

    feat: cover core concepts

--- React/my-react-app/README.md
+++ React/my-react-app/README.md
@@ -4,4 +4,5 @@ Topics:
 1. Components (Convered)
 2. Props (Convered)
 3. Styles (Convered)
-4. State & Hooks
+4. State & Hooks (Most important state) (Covered)
+5. Useffect (covered)
--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+
+
+// const hasLiked = true;
 
 
 const Card = ({title}) => {
+
+  const [count , setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked])
+
+  useEffect(() => {
+    console.log('CARD RENDERED')
+  }, [])
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      
+      <h2>{title} <br /> {count || null}</h2>
+      
+      {/* <button onClick={() => setHasLiked(true)}> */}
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? 'Unlike' : 'Like'}
+      </button>
     </div>
   )
 }
@@ -25,6 +47,9 @@ const Card = ({title}) => {
 // }
 
 const App = () => {
+  
+
+
   return  (
     <>
       <div className="card-container">
